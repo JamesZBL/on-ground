320be8d41c0b85f8974a2cf782373ad4cb5086f9
zhengbaole <zhengbaole@xdfzx.com>
2026-01-19 13:31:52 +0800

    API key

--- build.js
+++ build.js
@@ -17,6 +17,8 @@ if (!apiKey) {
     console.warn('   示例: BAIDU_API_KEY=your_key npm run build');
 }
 
+console.log('apiKey is', apiKey);
+
 // 读取配置模板
 const configTemplate = fs.readFileSync(path.join(__dirname, 'config.template.js'), 'utf8');
 
